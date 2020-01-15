b6952f02c5d1b8d80303b9b2dc4a1d266d9e6e59
Iryna_Baranov <irina.panasiuk1@gmail.com>
2020-01-15 17:58:45 +0200

    POM: added object locator

---
+++ src/test/java/test/GoogleSearchTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.GoogleSearchPage;
+
+public class GoogleSearchTest {
+    private static WebDriver driver = null;
+
+    public static void main(String[] args) {
+        googleSearch();
+
+    }
+
+    private static void googleSearch(){
+
+        String projectPath = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver", projectPath +"/Drivers/chromeriver/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+        //driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Hello World");
+
+        GoogleSearchPage.textbox_search(driver).sendKeys("Hello World");
+        GoogleSearchPage.textbox_search(driver).sendKeys(Keys.RETURN);
+
+        //driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+
+        driver.close();
+
+        System.out.println("Test Completed Successfully");
+
+    }
+}
